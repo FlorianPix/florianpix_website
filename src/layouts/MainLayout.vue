ab7d33d017e510f607d9b45b8cd9f5af8c91c143
Florian Pix <florian.pix.97@outlook.com>
2023-02-06 11:09:49 +0100

    made toolbar responsive;

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="bg-dark">
+      <!-- desktop toolbar -->
+      <q-toolbar class="bg-dark gt-sm">
         <q-toolbar-title>
           <a href="" class="handwritten">
             Florian Pix
@@ -14,10 +15,49 @@
           <q-route-tab to="/contact" name="contact" v-bind:label="$t('tabs.contact')" />
         </q-tabs>
 
-        <q-select
-          v-model="$i18n.locale"
-          :options="['en-US', 'de']"
-        />
+        <div v-if="languageSettingsOpen">
+          <q-btn round class="q-ma-xs" @click="$i18n.locale='de'">
+            🇩🇪
+          </q-btn>
+          <q-btn round class="q-ma-xs" @click="$i18n.locale='en-US'">
+            🇺🇸
+          </q-btn>
+        </div>
+
+        <q-btn round icon="settings" @click="toggleLanguageSettings">
+        </q-btn>
+      </q-toolbar>
+
+      <!-- mobile toolbar -->
+      <q-toolbar class="bg-dark lt-md">
+        <q-btn round icon="menu" @click="toggleTabs">
+        </q-btn>
+
+        <q-toolbar-title>
+          <a href="" class="handwritten">
+            Florian Pix
+          </a>
+        </q-toolbar-title>
+
+        <div v-if="languageSettingsOpen">
+          <q-btn round class="q-ma-xs" @click="$i18n.locale='de'">
+            🇩🇪
+          </q-btn>
+          <q-btn round class="q-ma-xs" @click="$i18n.locale='en-US'">
+            🇺🇸
+          </q-btn>
+        </div>
+
+        <q-btn round icon="settings" @click="toggleLanguageSettings">
+        </q-btn>
+      </q-toolbar>
+
+      <q-toolbar class="bg-dark" v-if="tabsOpen">
+        <q-tabs v-model="tab">
+          <q-route-tab to="/projects" name="projects" v-bind:label="$t('tabs.projects')" />
+          <q-route-tab to="/about" name="about" v-bind:label="$t('tabs.about')" />
+          <q-route-tab to="/contact" name="contact" v-bind:label="$t('tabs.contact')" />
+        </q-tabs>
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -28,7 +68,6 @@
 
 <script lang="ts">
 import {defineComponent, ref} from 'vue';
-
 export default defineComponent({
   name: 'MainLayout',
 
@@ -36,9 +75,23 @@ export default defineComponent({
 
   setup () {
     const tab = ref('about')
+    const languageSettingsOpen = ref(false)
+    const tabsOpen = ref(false)
+
+    const toggleLanguageSettings = () => {
+      languageSettingsOpen.value = !languageSettingsOpen.value;
+    }
+
+    const toggleTabs = () => {
+      tabsOpen.value = !tabsOpen.value;
+    }
 
     return {
+      languageSettingsOpen,
       tab,
+      tabsOpen,
+      toggleLanguageSettings,
+      toggleTabs
     }
   }
 });
